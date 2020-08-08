df2aedef2e7df19d51947b2c8e0bf32f7eaaf902
Prince Sharma <princesharma.pri@gmail.com>
2020-08-08 20:53:33 +0530

    added STOP->ENTER , ADD->CLICK , RANDOM-> r , CLEAR-> w, feature

--- sketch.js
+++ sketch.js
@@ -6,6 +6,9 @@
 // Game of Life 
 // Time -> O(m*n) Space -> O(1)
 // made using p5.js 
+// mark die->live , -1
+// mark live->die , 2
+// update -1 => 1 , 2 => 0 
 
 function make2DArray(cols, rows) {
   let arr = new Array(cols);
@@ -18,12 +21,20 @@ function make2DArray(cols, rows) {
 let grid;
 let cols;
 let rows;
-let resolution = 10;
+let resolution = 40;
+let paused;
+let editing;
+let selectX;
+let selectY;
+let randKeyPressed;
 
 function setup() {
-  createCanvas(700, 400);    // full screen size 1890, 940
+  createCanvas(1600, 800);    // full screen size 1890, 940
   cols = width / resolution;
   rows = height / resolution;
+  paused = false ;
+  editing = false ;
+  randKeyPressed = false ;
 
   grid = make2DArray(cols, rows);
   for (let i = 0; i < cols; i++) {
@@ -34,48 +45,15 @@ function setup() {
 }
 
 function draw() {
-  background(0);
-
-  for (let i = 0; i < cols; i++) {
-    for (let j = 0; j < rows; j++) {
-      let x = i * resolution;
-      let y = j * resolution;
-      if (grid[i][j] == 1) {
-        fill(20, 148, 20);
-        stroke(0);
-        rect(x, y, resolution - 1, resolution - 1);
-      }
-    }
-  }
-
-
-  for (let i = 0; i < cols; i++) {
-    for (let j = 0; j < rows; j++) {
-      let state = grid[i][j];
-      // Count live neighbors!
-      let neighbors = countNeighbors(grid, i, j);
-
-      if (state == 0 && neighbors == 3) {
-        grid[i][j] = -1;
-      }
-      
-      if (state == 1 && (neighbors < 2 || neighbors > 3)) {
-        grid[i][j] = 2;
-      }
-    }
-  }
-
-  for ( let i = 0 ; i < cols ; i++) {
-    for (let j = 0; j < rows; j++) {
-      if (grid[i][j] == -1 ) {
-        grid[i][j] = 1 ;
-      } 
-
-      if (grid[i][j] == 2) {
-        grid[i][j] = 0;
-      }
-    }
-  }
+  
+  if (!paused) {
+    fillGrid();
+    processOneStep();
+    updateGrid();
+  } else {
+    fillGrid();
+    editing = true ;
+  }  
 }
 
 function countNeighbors(grid, x, y) {
@@ -92,4 +70,95 @@ function countNeighbors(grid, x, y) {
     }
   } 
   return sum;
+}
+
+function fillGrid() {
+
+  background(0);  // 0
+
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      let x = i * resolution;
+      let y = j * resolution;
+      if (grid[i][j] == 1) {
+        fill(20, 148, 20);   // 20, 148, 20 
+        stroke(0);
+        rect(x, y, resolution - 1, resolution - 1);
+      }
+    }
+  }
+
+}
+
+function updateGrid() {
+  for ( let i = 0 ; i < cols ; i++) {
+    for (let j = 0; j < rows; j++) {
+      if (grid[i][j] == -1 ) {
+        grid[i][j] = 1 ;
+      } 
+
+      if (grid[i][j] == 2) {
+        grid[i][j] = 0;
+      }
+    }
+  }
+}
+
+function processOneStep() {
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      let state = grid[i][j];
+      // Count live neighbors!
+      let neighbors = countNeighbors(grid, i, j);
+
+      if (state == 0 && neighbors == 3) {
+        grid[i][j] = -1;
+      }
+      
+      if (state == 1 && (neighbors < 2 || neighbors > 3)) {
+        grid[i][j] = 2;
+      }
+    }
+  }
+}
+
+function keyTyped() {
+  // 13 represents the 'enter' key
+  if ( keyCode == 13 ){
+    if (!paused) {
+      paused = true;
+    } else if (paused) {
+      paused = false;
+    }
+  }
+
+  if (key == 'r' ) {
+    for (let i = 0; i < cols; i++) {
+      for (let j = 0; j < rows; j++) {
+        grid[i][j] = floor(random(2));
+      }
+    }
+    randKeyPressed = true ;
+  }
+
+  if ( key == 'w' && paused ) {
+    for (let i = 0; i < cols; i++) {
+      for (let j = 0; j < rows; j++) {
+        grid[i][j] = 0;
+      }
+    }
+  }
+}
+
+
+function mousePressed() {
+  console.log("mouse Pressed");
+  selectX = floor(mouseX / resolution );
+  selectY = floor(mouseY / resolution );
+  console.log(selectX,selectY);
+  if (grid[selectX][selectY] == 0 || grid[selectX][selectY] == -1 ) {
+    grid[selectX][selectY] = 1 ;
+  } else {
+    grid[selectX][selectY] = 0 ;
+  }
 }
